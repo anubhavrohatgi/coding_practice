229dc77053c37da4ac487f845f14f13cc7acd37e
Kwan-I Lee <KLee@walmartlabs.com>
2016-09-04 02:57:41 -0700

    2D array iterator

---
+++ cpp/Airbnb/2D_array_iterator.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class myIterator {
+private:
+	vector<vector<int>>::iterator it, end;
+	int ptr, removePtr;
+
+	const vector<vector<int>>::iterator head;
+	vector<vector<int>>::iterator print;
+public:
+	myIterator(vector<vector<int>> &data) : it(data.begin()), end(data.end()), ptr(0), removePtr(-1), head(data.begin()) {
+	}
+
+	int next() {
+		removePtr = ptr;
+		return (*it)[ptr++];
+	}
+
+	bool hasNext() {
+		while(it != end && ptr == (*it).size()) {
+			it++;
+			ptr = 0;
+		}
+
+		return it != end;
+	}
+
+	void remove() {
+		// TODO if removePtr == -1, throw exception 
+
+		// 2 cases: 	1. removePtr is pointing to the last element of current vector.
+		// 		2. removePtr is pointing to the middle element of current vector.
+		if(removePtr == (*it).size()-1) {
+			(*it).erase( (*it).begin() + removePtr );
+			removePtr = -1;
+		}
+		else {
+			(*it).erase( (*it).begin() + removePtr );
+			// vector has shrinked, move the pointer backward
+			ptr--;	
+		}
+	}
+
+	void printRawData() {
+		print = head;
+		int p = 0;
+		cout << "-----------" << endl;
+		while(print != end) {
+			while(ptr < (*print).size()) {
+				cout << (*print)[ptr++] << " ";
+			}
+			cout << endl;
+			print++;
+			ptr = 0;
+		}
+	}
+};
+
+int main() {
+	vector<vector<int>> data = {
+		{1, 4, 2, 5},
+		{7, 2, 4},
+		{},
+		{9}
+	};
+
+	myIterator it = myIterator(data);
+	while(it.hasNext()) {
+//		cout << it.next() << endl;
+		it.next();
+		it.remove();
+		it.printRawData();
+	}
+
+}
