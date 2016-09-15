39bb7a94bdf3ab49e4d60b4dae01b4367d38c09d
Kwan-I Lee <KLee@walmartlabs.com>
2016-09-15 01:05:23 -0700

    handle duplicates case

--- cpp/Facebook/random_pop_list.cpp
+++ cpp/Facebook/random_pop_list.cpp
@@ -35,6 +35,53 @@ public:
 	}
 };
 
+// This will handles rList with no duplicated element
+list<int> popAndReconstructWithDup(RandomPopList &rList) {
+	list<int> small, large, dup;
+
+	int prev = INT_MAX;
+	while(!rList.empty()) {
+		int curr = rList.pop();
+
+		if(prev == INT_MAX) {
+			prev = curr;
+			continue;
+		}
+
+		if(curr > prev) {
+			if(dup.empty())
+				small.push_back(prev);
+			else {
+				dup.push_back(prev);
+				small.insert(small.end(), dup.begin(), dup.end());
+				dup.clear();
+			}
+		}
+		else if(curr < prev) {
+			if(dup.empty())
+				large.push_front(prev);
+			else {
+				dup.push_back(prev);
+				large.insert(large.begin(), dup.begin(), dup.end());
+				dup.clear();
+			}
+		}
+		else {	// curr == prev
+			dup.push_back(prev);
+		}
+
+		prev = curr;
+	}
+
+	if(!dup.empty()) {
+		small.insert(small.end(), dup.begin(), dup.end());
+	}
+	small.push_back(prev);
+
+	small.insert(small.end(), large.begin(), large.end());
+	return small;
+}
+
 // This only handles rList with no duplicated element
 list<int> popAndReconstruct(RandomPopList &rList) {
 	list<int> small, large;
@@ -80,4 +127,16 @@ int main() {
 	list<int> new_lst = popAndReconstruct(rList);
 	cout << "popAndReconstruct()" << endl;
 	print_list(new_lst);
+
+	// ---------
+
+	list<int> lst2 = {1, 2, 3, 4, 6, 6, 6, 6, 6, 6, 8, 9, 9, 9, 9, 14, 16, 20};
+	RandomPopList rList2(lst2);
+
+	cout << "Original list : " << endl;
+	print_list(lst2);
+
+	list<int> new_lst2 = popAndReconstructWithDup(rList2);
+	cout << "popAndReconstructWithDup()" << endl;
+	print_list(new_lst2);
 }
